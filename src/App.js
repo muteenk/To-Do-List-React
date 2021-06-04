1485ec686ac8f656376ed433430e98f1959a8645
MuteenK <kundangarmuteen@protonmail.com>
2021-06-04 19:25:20 +0530

    designed the add task bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './components/Nav'
+import AddTodo from './components/AddTodo'
 
 function App() {
   return (
     <>
       <Nav/>
+      <AddTodo/>
     </>
   );
 }
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddTodo = () => {
+    return (
+        <div className="makeToDo">
+            <input type="text" name="AddToDo" placeholder="Enter Task" />
+            <button> Add Task </button>
+        </div>
+    )
+}
+
+export default AddTodo;
